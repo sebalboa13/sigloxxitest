93a071a627f536155ac6ca93c2ba116b5526db43
sebalboa13 <87154026+sebalboa13@users.noreply.github.com>
2021-10-08 02:05:46 -0300

    gg

--- Siglo_XXI/settings.py
+++ Siglo_XXI/settings.py
@@ -138,3 +138,8 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
