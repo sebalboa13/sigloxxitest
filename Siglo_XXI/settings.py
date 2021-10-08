49775fcca9b89c777dfb5ab5b15faf4c2dbdc4e9
sebalboa13 <87154026+sebalboa13@users.noreply.github.com>
2021-10-08 01:59:35 -0300

    dsfs

--- Siglo_XXI/settings.py
+++ Siglo_XXI/settings.py
@@ -119,9 +119,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
